3aba9910b725083ea01dd2f7379e31bca83629c0
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-11-12 20:55:08 +0530

    Add files via upload

---
+++ board-Oct/datatype.js
@@ -0,0 +1,142 @@
+//There are 8 datatypes in js
+
+/* let message = 'hello'
+
+message = 20 */
+
+
+//Number
+//It represents both integer and 
+//float
+
+/* let num = 10
+
+console.log(num * 2) */
+
+//multiplacation, addition, substration, division and many more
+
+
+//Number data type also includes special numeric values
+
+//Infinity, -Infinity, NaN
+
+/* console.log(1/0) */
+/* console.log(Infinity)
+ */
+
+/* console.log( 'hello'/2)
+ */
+/* console.log(NaN**0) an exception */
+
+
+
+/* BigInt */
+
+/* if the range of an integer is
+
++- (2power53 - 1)
+*/
+/*
+const bigInt = 63786877672367848732648732648732646372864236432786478236478236487362874682376423876478364782467826784623n;
+
+console.log(bigInt) */
+
+/* String */
+
+//a string in js is always surrounded by  quotes
+
+/* let string = 'hello'
+
+let string2 = "hi"
+
+let string3 = `bye` */
+/* console.log(string, string2, string3) */
+
+
+//there is no difference in double and single quotes
+
+//Bacticks special quotes for extended functionality
+/*
+
+let name = 'john'
+console.log("hello " + name)
+console.log(`hello ${name} `)
+
+
+console.log(`Sum is ${2 + 3} `)
+
+ */
+
+
+
+/* Boolean */
+
+//true or false
+
+/* let isAdmin = true
+
+console.log(isAdmin) */
+
+/* console.log( 2 < 3) */
+
+/* null data type
+
+let age = null
+
+it just represents nothing, empty, value that is unknown
+
+age=12
+
+console.log(age)
+
+ */
+
+/* undefined */
+
+
+//the meaning of undefined is not assigned
+
+let age;
+console.log(age)
+age = 10
+console.log(age)
+
+/* Type conversion */
+
+
+//String conversion
+
+/* let value = true
+console.log(typeof value)
+
+value = String(value)
+
+console.log(typeof value)
+
+
+"true" */
+
+/* let str = "1234"
+
+console.log(typeof str)
+
+
+let num = Number(str)
+
+
+console.log(typeof num) */
+
+
+let age = Number('this is a string')
+
+console.log(typeof age)
+
+
+
+
+
+
+
+
+
+
---
+++ board-Oct/variable.js
@@ -0,0 +1,96 @@
+
+
+// let username = 'John'
+// let age = 20
+// let message = 'hello'
+
+// let username = 'John', age = 20, message = 'hello'
+
+
+
+//single line comment
+
+
+
+/*multi line comments
+console.log('hi')
+
+ */
+
+/* this program does this*/
+console.log('bye')
+
+/* 
+let name = 'John'
+
+name = 'Peter'
+
+console.log(name) */
+
+/* 
+let name = 'john'
+
+let firstName;
+
+firstName = name
+
+console.log(name)
+console.log(firstName) */
+
+
+
+//let name = 'john'
+
+//let name = 'abcd'
+
+//Variable Namings
+
+/* 1. the name must contain only letters, digits or symbols  $ and _
+
+2. the first character cannot be a digit
+*/
+
+/* let username
+let test123 */
+/* let $ = 2
+let _ = 20
+
+console.log($, _) */
+
+/* invalid names */
+/* let 2a = 'hello'
+ */
+//let my-name
+
+
+/*  let myfirstname = 'john'
+ */
+//camel case or pascal case
+
+//let myFirstName is camelcase
+
+//let MyFirstName is pascal case
+
+//case sesitive language
+
+
+/* let myname = 'john'
+
+MYNAME
+myName
+MyName */
+
+
+//if you want to declare a constant(unchanging) variable
+
+const myBirthday = '08/10/2000'
+
+
+
+
+
+
+
+
+
+
