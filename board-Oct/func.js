8a02a23108cded4df0f2e5f27f4d5e613d29478d
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-11-27 21:00:29 +0530

    Add files via upload

---
+++ board-Oct/func.js
@@ -0,0 +1,85 @@
+//Function Declaration
+
+// function name(){}
+
+//print the numbers from 1-5
+
+// function printValuesFromOneToFive() {
+
+//     let i = 1
+
+//     while (i <= 5) {
+
+//         console.log(i)
+//         i++
+//     }
+// }
+// printValuesFromOneToFive() //calling or function invocation
+
+// console.log('hello world')
+// //100 lines of code
+
+// printValuesFromOneToFive()
+
+
+
+
+// console.log('hello')
+// function x() {
+//     console.log('greetings')
+// }
+// x()
+// console.log('bye')
+
+
+// function getSum(a, b) {
+
+//     if (a == 10 || a == 2) {
+//         return
+//     }
+
+//     console.log('a is ', a)
+//     console.log('b is ', b)
+
+//     let sum = a + b
+//     console.log(sum)
+
+// }
+// getSum(10, 20) //arguments
+// getSum(5, 10)
+// getSum(2, 3)
+
+
+function getSum(a, b) {
+
+
+    return (a + b)
+
+    console.log('hi')
+
+    //return is a statement that ends a function execution and it returns the value to the function caller
+}
+
+console.log(getSum(100, 200))
+console.log('hello world')
+
+
+//memory creation phase
+//assigns memory to  all the variables and function
+
+
+var num = 10
+
+function x() {
+
+    var num = 20
+    console.log(num)
+
+}
+console.log(num)
+
+x()
+console.log('bye everyone')
+
+
+
