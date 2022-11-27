e6f10761388a511201e39af936c46ed681b4bef8
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-11-27 15:52:35 +0530

    Add files via upload

---
+++ board-Oct/summary.js
@@ -0,0 +1,218 @@
+/* console.log('My name is John ')
+
+//single line comment
+//console.log(10)
+
+//multiple line comment
+console.log(20)
+console.log(30) */
+
+
+//Variables
+
+//declared a new variable
+/* let message;
+
+//initialize a variable
+message = 'hello world'
+
+console.log(message) */
+
+/* let num;
+num = 10
+console.log(num) */
+
+
+/* 
+
+let message = 'hello world'
+
+console.log(message) */
+
+//multiple variables
+/* 
+let user = 'John';
+let age = 20;
+let message = 'Hi all'; */
+
+let user = 'John', age = 20, message = 'Hi all'
+
+console.log(message)
+
+
+
+let message;
+
+message = 'Hello';
+console.log(message)
+message = 'World'
+
+console.log(message)
+
+
+
+/* let age = 500;
+ */let age = 20
+/* 
+let ageTwo = age
+
+console.log(age)
+console.log(ageTwo) */
+
+
+
+//Variable Naming
+
+//Two limitations
+//1.The name must contain only letters, digits, or the symbol $ and _
+//2.The first character cannot be a 
+//digit
+
+
+//valid variable names
+//case matters in javascript
+/* 
+let $ = 20
+let _ = 40 */
+
+/* let myfirstname = 'john' */
+
+/* let myFirstName = 'john' //camelCase */
+/* let test123 = 'test' */
+
+/* let 123a = 'hello'
+ */
+/*  let my-name = 'john'
+ */
+
+//Reserved names 
+/* 
+let let = 10
+let return = 20
+let function = 40 */
+
+
+//const  Constants
+//if you want to declare a constant (unchanging) variable use const instead of let
+
+const myBirthday = '10.05.2000'
+
+console.log(myBirthday)
+
+
+
+//Data Types
+
+//8 basic types in javascript
+
+
+//js is a dynamically typed languge
+/* let message = 'hello'
+
+message = 20
+
+console.log(message)
+ */
+
+
+//Number
+//it contains both integers and floating point numbers
+
+/*  let num = 20
+  num = 10.234
+ console.log(num)
+ console.log(typeof num)  */
+
+//Special numeric values
+
+/* 
+console.log(1 / 0) */
+
+/*  console.log(Infinity) */
+
+/* console.log('hello'/2 + 2) results to NaN
+ */
+
+//Big Int
+
+
+/* const bigInt = 876387163871263127836812763876312876371283782163782163782163781263786371263721863712863721637863876217863782163871263712863781263781263783278638n
+
+console.log(bigInt) */
+
+
+
+//String
+
+// 3 types of quotes like single quotes, double quotes,
+//backticks => "EXTENDED FUNCTIONALITY"
+
+let text = 'hello'
+
+let text2 = "Using double quotes "
+
+let text3 = `Using back ticks ${text}  ${text2}`
+console.log(text3)
+/*  console.log(typeof text) */
+
+/* console.log(text + " " + text2) */
+
+
+
+ //Boolean => true or false
+ 
+/*  
+ let nameFieldChecked = true
+ 
+ let ageFieldChecked = false */
+ 
+ 
+/* let isGreater = 4 > 1
+
+ */
+ 
+ //null undefined ,objects
+ 
+ 
+ 
+ //conditionals
+/* 
+let breakfastTime = 10
+
+let wokeUpTime = 12
+
+if( wokeUpTime < breakfastTime){
+
+  console.log('Your breakfast is ready')
+
+}else{
+
+  console.log('Come tomorrow')
+
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
