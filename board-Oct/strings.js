c744dd153cc17a6439bf5ae1537c9329bf4595c3
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-12-03 20:51:37 +0530

    Add files via upload

---
+++ board-Oct/strings.js
@@ -0,0 +1,11 @@
+//Strings and its methods
+//""  , '', ``
+/* 
+function sum(a,b){
+
+  return a + b
+
+}
+
+console.log(`The value of 10 + 2 =  ${sum(10,2)}`)
+ */
---
+++ board-Oct/varLetConst.js
@@ -0,0 +1,65 @@
+function x() {
+    //     function y() {
+    //         function z() {
+    //             // const a = 20 //its scope is limited
+    //             console.log(b)
+    //         }
+    //         z()
+    //     }
+    //     y()
+    // }
+    // x()
+    // var b = 200
+    // //lexical scope , environment
+
+
+    //hoisting is a process where you can get the value of function, classes, variables even before execution of  the code
+
+// b()
+// function b() {
+
+// }
+
+// console.log(a)
+// var a = 100
+
+// console.log(a)
+// let a = 100
+
+// console.log(a)
+// const a = 100
+
+//the time from which you cannot use a variable declared by let or const till the time it is initialized i.e called as temporal dead one
+
+
+
+//var is a function scope whereas let and const are blocked scope
+// var a = 10 
+// {
+//     var a = 20 // shawdowing
+// }
+// console.log(a)
+// let a = 10 // blocked scoped
+// {
+//     let a = 20
+// }
+// console.log(a)
+
+
+
+
+// var a = 100
+
+// function x() {
+//     var a = 200
+//     console.log(a)
+// }
+// x()
+
+
+// const a = 10
+
+// a = 20
+
+// console.log(a)
+
